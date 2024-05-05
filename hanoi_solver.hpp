4b48ba5cd0e431e53b81b8e7535c420871096a1a
Nicholas Lenzo <nicholas.lenzo@stonybrook.edu>
2024-05-04 23:51:56 -0400

    Simple description of function

--- hanoi_solver.hpp
+++ hanoi_solver.hpp
@@ -1,6 +1,10 @@
 #ifndef __HANOI_SOLVER_H__
 #define __HANOI_SOLVER_H__
-// Hanoi solver
-int hanoi_solver(int, int, int, int);
+
+/// 
+/// Print solution of the game
+///
+
+int hanoi_solver(int start, int middle, int end , int ndisks);
 
 #endif
